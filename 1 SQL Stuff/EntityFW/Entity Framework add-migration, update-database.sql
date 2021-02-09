020e4b61ec6b9e5c8cff14e6a87c6ea5b9dc8cf9
mihai <mihail.plugaru@outlook.com>
2021-02-09 14:13:51 +0200

    HTML CSS CV page

--- 1 SQL Stuff/EntityFW/Entity Framework add-migration, update-database.sql
+++ 1 SQL Stuff/EntityFW/Entity Framework add-migration, update-database.sql
@@ -2,11 +2,10 @@ select * from Product;
 select * from Stock;
 select * from OrderItem;
 
-drop table Product;
 drop table Stock;
 drop table OrderItem;
+drop table Product;
 
 drop database ShopDb
-
 
 create database ShopDb
